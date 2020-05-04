bad61c4783a852159702e73f7c4bffdd1c7bd5c8
Cynaith <894324762@qq.com>
2020-05-05 02:36:18 +0800

    增加set命令

--- src/main/java/com/github/lightredis/jedis/Ledis.java
+++ src/main/java/com/github/lightredis/jedis/Ledis.java
@@ -1,5 +1,8 @@
 package com.github.lightredis.jedis;
 
+import com.github.lightredis.util.ObjectUtil;
+import com.github.lightredis.util.SerializeObjectTool;
+import com.sun.xml.internal.ws.encoding.soap.SerializationException;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.ScanParams;
 import redis.clients.jedis.ScanResult;
@@ -12,11 +15,11 @@ import java.util.List;
  * @DATE: 2020/5/3
  * refer : https://blog.csdn.net/weixin_34152820/article/details/86443458
  **/
-public class Ledis implements LedisCommands{
+public class Ledis implements LedisCommands {
 
-    Jedis jedis ;
+    Jedis jedis;
 
-    private Ledis(){
+    private Ledis() {
 
     }
 
@@ -24,7 +27,7 @@ public class Ledis implements LedisCommands{
         this.jedis = jedis;
     }
 
-    public static Ledis getInstance(){
+    public static Ledis getInstance() {
         LedisInterceptor ledisInterceptor = new LedisInterceptor();
         return ledisInterceptor.getLedis();
     }
@@ -57,18 +60,54 @@ public class Ledis implements LedisCommands{
         return jedis.del(key);
     }
 
-    public List<String> scan(String prefix,String suffix) {
+    public List<String> scan(String prefix, String suffix) {
 
-        String scanCursor= "0";
+        String scanCursor = "0";
         ScanParams scanParams = new ScanParams();
         scanParams.count(3000);
         List<String> resultList = new ArrayList<String>();
-        do{
-            ScanResult result = jedis.scan(scanCursor,scanParams.match(prefix+"*"+suffix));
+        do {
+            ScanResult result = jedis.scan(scanCursor, scanParams.match(prefix + "*" + suffix));
             scanCursor = result.getStringCursor();
             resultList.addAll(result.getResult());
-        }while (!(scanCursor.equals("0")));
+        } while (!(scanCursor.equals("0")));
         return resultList;
     }
 
+    public Long setnx(String key, String value) {
+        return jedis.setnx(key, value);
+    }
+
+    public List<String> mget(String... key) {
+        return jedis.mget(key);
+    }
+
+    /**
+     * 当key不存在时，返回emptyValue
+     * @param emptyValue
+     * @param key
+     * @return
+     */
+    public List<String> mgets(String emptyValue, String... key) {
+        List<String> results = new ArrayList<String>();
+        for (int i = 0; i < key.length; i++) {
+            String result = jedis.get(key[i]);
+            try {
+                if (ObjectUtil.isEmpty(result)){
+                    results.add(emptyValue);
+                }
+                else results.add(result);
+            } catch (NullPointerException e) {
+
+            }
+        }
+        return results;
+    }
+
+    public String setObj(String key, Object value) {
+        if (value instanceof java.io.Serializable)
+            throw new SerializationException("object should be serialization");
+        jedis.set(key.getBytes(), SerializeObjectTool.serialize(value));
+        return "OK";
+    }
 }
