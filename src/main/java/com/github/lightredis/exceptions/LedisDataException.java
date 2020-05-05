c46d845388e2ceb94fb1bcd1a448ad436a9b5832
Cynaith <894324762@qq.com>
2020-05-06 02:41:17 +0800

    增加 异常类

---
+++ src/main/java/com/github/lightredis/exceptions/LedisDataException.java
@@ -0,0 +1,15 @@
+package com.github.lightredis.exceptions;
+
+/**
+ * @USER: lynn
+ * @DATE: 2020/5/5
+ **/
+public class LedisDataException extends RuntimeException{
+    public LedisDataException() {
+        super();
+    }
+
+    public LedisDataException(String message) {
+        super(message);
+    }
+}
