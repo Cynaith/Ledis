0e0810123da78153a7fb0ff15a94f6b741f8e5cb
Cynaith <894324762@qq.com>
2020-05-06 15:24:13 +0800

    Update StringTest.java

--- src/main/java/test/StringTest.java
+++ src/main/java/test/StringTest.java
@@ -1,8 +1,14 @@
 package test;
 
 import com.github.lightredis.jedis.Ledis;
+import com.github.lightredis.util.CutTime;
+import com.github.lightredis.util.SerializeObjectTool;
 
-import java.util.List;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @USER: lynn
@@ -16,19 +22,12 @@ public class StringTest {
      */
 //    public static void main(String[] args) {
 //        CutTime cutTime = CutTime.getInstance();
-//        List<String> result ;
 //        cutTime.startTime();
-//        Ledis ledis = Ledis.getInstance();
-//        result = ledis.scan("ledis", "7");
-//        System.out.println(cutTime.endTime());
-//        Iterator<String> iterator = result.listIterator();
-//        while (true){
-//            if (iterator.hasNext()){
-//                System.out.println(iterator.next());
-//            }
-//            else break;
+//        Ledis ledis = new Ledis("127.0.0.1",6379);
+//        for (int i = 0; i < 100000; i++) {
+//            ledis.set("ledis:num" + i, String.valueOf(i));
 //        }
-//
+//        System.out.println(cutTime.endTime());
 //    }
     /**
      * 测试Jedis操作
@@ -38,23 +37,62 @@ public class StringTest {
 //public static void main(String[] args) {
 //    CutTime cutTime = CutTime.getInstance();
 //    cutTime.startTime();
-////    JedisPoolConfig config = new JedisPoolConfig();
-////    config.setMaxIdle(5);
-////    config.setMaxTotal(500);
-////    config.setMaxWaitMillis(1000*100);
-////    config.setTestOnBorrow(true);
-////    JedisPool pool = new JedisPool(config,"127.0.0.1",6379,10000);
-////    Jedis jedis = pool.getResource();
-//    Jedis jedis = new Jedis("127.0.0.1",6379);
+//    JedisPoolConfig config = new JedisPoolConfig();
+//    config.setMaxIdle(20);
+//    config.setMaxTotal(100);
+//    config.setMaxWaitMillis(1000*100);
+//    config.setTestOnBorrow(true);
+//    JedisPool pool = new JedisPool(config,"127.0.0.1",6379,10000);
+//
+////    Jedis jedis = new Jedis("127.0.0.1",6379);
 //    for (int i=0;i<100000;i++){
+//        Jedis jedis = pool.getResource();
 //        jedis.set("ledis:num"+i,String.valueOf(i));
+//        jedis.close();
 //    }
 //    System.out.println(cutTime.endTime());
 //}
 
-    public static void main(String[] args) {
-       Ledis ledis = Ledis.getInstance();
-       List<String> list = ledis.mgets("empty","ledis:num1","ledis:nu2");
-        System.out.println(list.get(0)+list.get(1));
+    /**
+     * SortedSet
+     * @param args
+     */
+//    public static void main(String[] args) {
+//        Ledis ledis = new Ledis("127.0.0.1",6379);
+//        ledis.zadd("sort",1,"liu");
+//        ledis.zadd("sort",2,"li");
+//        ledis.zadd("sort",3,"wng");
+//        ledis.zadd("sort",4,"zheng");
+//        ledis.zadd("sort",5,"wang");
+//        Map<String,Double> strings = ledis.zrangeWithScores("sort",2,4);
+//        System.out.println(ledis.zcount("sort",2,5));
+//
+//    }
+
+}
+
+class Car implements Serializable {
+
+    private static final long serialVersionUID = -5927431407988171786L;
+
+    String color;
+
+    public Car(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                '}';
     }
 }
