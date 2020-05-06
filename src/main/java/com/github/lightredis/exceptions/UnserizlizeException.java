1732c57bfd4566ed0a841b64a696edae590f97e5
Cynaith <894324762@qq.com>
2020-05-06 15:23:13 +0800

    增加反序列化异常类

---
+++ src/main/java/com/github/lightredis/exceptions/UnserizlizeException.java
@@ -0,0 +1,16 @@
+package com.github.lightredis.exceptions;
+
+/**
+ * @USER: lynn
+ * @DATE: 2020/5/6
+ **/
+public class UnserizlizeException extends RuntimeException{
+
+    public UnserizlizeException() {
+        super();
+    }
+
+    public UnserizlizeException(String message) {
+        super(message);
+    }
+}
