99531f35fbf56066b1a2d3118163642d71d6ee00
Cynaith <894324762@qq.com>
2020-05-06 15:24:10 +0800

    Update LedisInterceptor.java

--- src/main/java/com/github/lightredis/jedis/LedisInterceptor.java
+++ src/main/java/com/github/lightredis/jedis/LedisInterceptor.java
@@ -1,6 +1,7 @@
 package com.github.lightredis.jedis;
 
 import com.github.lightredis.util.JedisPool;
+import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import redis.clients.jedis.Jedis;
@@ -17,16 +18,28 @@ public class LedisInterceptor implements MethodInterceptor {
 
     private Jedis jedis;
 
-    public LedisInterceptor(){
-        jedis = JedisPool.getJedis();
-        ledis = new Ledis(jedis);
+    public LedisInterceptor(Ledis ledis){
+
+        this.ledis = ledis;
     }
 
     public Ledis getLedis(){
         return ledis;
     }
 
+    public Object getObject() {
+        Enhancer enhancer = new Enhancer();
+        Class<?> clazz = this.ledis.getClass();
+        enhancer.setCallback(this);
+        enhancer.setClassLoader(clazz.getClassLoader());
+        enhancer.setInterfaces(clazz.getInterfaces());
+        enhancer.setSuperclass(clazz);
+        return enhancer.create();
+    }
+
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        jedis = JedisPool.getJedis();
+        ledis.setJedis(jedis);
         method.invoke(ledis,objects);
         jedis.close();
         return null;
