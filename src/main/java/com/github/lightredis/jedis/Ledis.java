8eccbdf6970d0e1d3489b01c25f5e014793c60c3
Cynaith <894324762@qq.com>
2020-05-06 15:23:47 +0800

    实现 SortedSet

--- src/main/java/com/github/lightredis/jedis/Ledis.java
+++ src/main/java/com/github/lightredis/jedis/Ledis.java
@@ -1,12 +1,14 @@
 package com.github.lightredis.jedis;
 
 import com.github.lightredis.exceptions.LedisDataException;
+import com.github.lightredis.exceptions.UnserizlizeException;
 import com.github.lightredis.util.ObjectUtil;
 import com.github.lightredis.util.SerializeObjectTool;
 import com.sun.xml.internal.ws.encoding.soap.SerializationException;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.ScanParams;
 import redis.clients.jedis.ScanResult;
+import redis.clients.jedis.Tuple;
 import redis.clients.jedis.exceptions.JedisDataException;
 
 import java.util.*;
@@ -20,15 +22,15 @@ public class Ledis implements LedisCommands {
 
     Jedis jedis;
 
-    public Ledis(String host,int port) {
-        jedis = new Jedis(host,port);
+    public Ledis(String host, int port) {
+        jedis = new Jedis(host, port);
     }
 
-    public Ledis(String host,int port,String password) {
-        jedis = new Jedis(host,port);
-        try{
+    public Ledis(String host, int port, String password) {
+        jedis = new Jedis(host, port);
+        try {
             jedis.auth(password);
-        }catch (JedisDataException e){
+        } catch (JedisDataException e) {
             throw new LedisDataException("ERR Client sent AUTH, but no password is set");
         }
     }
@@ -98,6 +100,7 @@ public class Ledis implements LedisCommands {
 
     /**
      * 当key不存在时，返回emptyValue
+     *
      * @param emptyValue
      * @param key
      * @return
@@ -107,10 +110,9 @@ public class Ledis implements LedisCommands {
         for (int i = 0; i < key.length; i++) {
             String result = jedis.get(key[i]);
             try {
-                if (ObjectUtil.isEmpty(result)){
+                if (ObjectUtil.isEmpty(result)) {
                     results.add(emptyValue);
-                }
-                else results.add(result);
+                } else results.add(result);
             } catch (NullPointerException e) {
 
             }
@@ -127,11 +129,11 @@ public class Ledis implements LedisCommands {
         }
     }
 
-    public Object getObj(String key){
+    public Object getObj(String key) {
         if (!jedis.exists(key.getBytes()))
             throw new NullPointerException("redis doesn't have the object you want");
         else {
-            byte[] value= jedis.get(key.getBytes());
+            byte[] value = jedis.get(key.getBytes());
             Object object = SerializeObjectTool.unserizlize(value);
             return object;
         }
@@ -144,62 +146,63 @@ public class Ledis implements LedisCommands {
     /**
      * 以下为List操作
      */
-    public Long lpush(String key, String... strings){
+    public Long lpush(String key, String... strings) {
         return jedis.lpush(key, strings);
     }
 
-    public Long rpush(String key,String... strings){
+    public Long rpush(String key, String... strings) {
         return jedis.rpush(key, strings);
     }
 
     /**
      * 从左边追加一个Object到list(key)中
+     *
      * @param key
      * @param objects
      * @return
      */
-    public Long lpush(String key,Object... objects){
+    public Long lpush(String key, Object... objects) {
         byte[][] bytes = new byte[objects.length][];
-        for(int i=0;i<objects.length;i++){
+        for (int i = 0; i < objects.length; i++) {
             bytes[i] = SerializeObjectTool.serialize(objects[i]);
         }
-        return jedis.lpush(key.getBytes(),bytes);
+        return jedis.lpush(key.getBytes(), bytes);
     }
 
     /**
      * 从右边追加一个Object到list(key)中
+     *
      * @param key
      * @param objects
      * @return
      */
-    public Long rpush(String key,Object... objects){
+    public Long rpush(String key, Object... objects) {
         byte[][] bytes = new byte[0][];
-        for(int i=0;i<objects.length;i++){
+        for (int i = 0; i < objects.length; i++) {
             bytes[i] = SerializeObjectTool.serialize(objects[i]);
         }
-        return jedis.rpush(key.getBytes(),bytes);
+        return jedis.rpush(key.getBytes(), bytes);
     }
 
-    public Long llen(String key){
+    public Long llen(String key) {
         return jedis.llen(key);
     }
 
-    public Object lindex(String key,int index){
-        return jedis.lindex(key,index);
+    public Object lindex(String key, int index) {
+        return jedis.lindex(key, index);
     }
 
     public Long lrem(String key, long count, Object value) {
-        if (value instanceof String){
-            return jedis.lrem(key,count,value.toString());
-        }
-        else {
+        if (value instanceof String) {
+            return jedis.lrem(key, count, value.toString());
+        } else {
             byte[] bytes = SerializeObjectTool.serialize(value);
-            return jedis.lrem(key.getBytes(),count,bytes);
+            return jedis.lrem(key.getBytes(), count, bytes);
         }
     }
 
-    public String ltrim(String key,int i, int j){
-        return jedis.ltrim(key,i,j);
+    public String ltrim(String key, int i, int j) {
+        return jedis.ltrim(key, i, j);
     }
 
     public String lpop(String key) {
@@ -210,31 +213,28 @@ public class Ledis implements LedisCommands {
         return jedis.rpop(key);
     }
 
-    public String lset(String key, int index,Object value) {
-        if (value instanceof String){
-            return jedis.lset(key,index,value.toString());
-        }
-        else {
+    public String lset(String key, int index, Object value) {
+        if (value instanceof String) {
+            return jedis.lset(key, index, value.toString());
+        } else {
             byte[] bytes = SerializeObjectTool.serialize(value);
-            return jedis.lset(key.getBytes(),index,bytes);
+            return jedis.lset(key.getBytes(), index, bytes);
         }
     }
 
     public List<String> sort(String key) {
-        if (jedis.llen(key)==0){
+        if (jedis.llen(key) == 0) {
             throw new LedisDataException("this list has no data");
-        }
-        else {
+        } else {
             return jedis.sort(key);
         }
 
     }
 
     public List<Object> sortObj(String key) {
-        if (jedis.llen(key)==0){
+        if (jedis.llen(key) == 0) {
             throw new LedisDataException("this list has no data");
-        }
-        else {
+        } else {
             List<byte[]> bytes = jedis.sort(key.getBytes());
             List<Object> objects = new ArrayList<Object>();
             for (int i = 0; i < bytes.size(); i++) {
@@ -250,24 +250,159 @@ public class Ledis implements LedisCommands {
 
         for (int i = 0; i < objects.length; i++) {
 
-            if (objects[i] instanceof String){
-                bytes[i] = ((String)objects[i]).getBytes();
-            }
-            else {
+            if (objects[i] instanceof String) {
+                bytes[i] = ((String) objects[i]).getBytes();
+            } else {
                 bytes[i] = SerializeObjectTool.serialize(objects[i]);
             }
         }
-        return jedis.sadd(key.getBytes(),bytes);
+        return jedis.sadd(key.getBytes(), bytes);
     }
 
     public Set<Object> smember(String key) {
         Set<Object> result = new HashSet<Object>();
         Set<byte[]> objects = jedis.smembers(key.getBytes());
         Iterator<byte[]> it = objects.iterator();
-        while (it.hasNext()) {
-            byte[] n = it.next();
-            result.add(SerializeObjectTool.unserizlize(n));
+        for (byte[] obj:objects){
+            try {
+                result.add(SerializeObjectTool.unserizlize(obj));
+            } catch (Exception e) {
+                result.add(new String(obj));
+            }
         }
         return result;
+    }
+
+
+    public Long srem(String key, Object... objects) {
+        byte[][] bytes = new byte[objects.length][];
+        for (int i = 0; i < objects.length; i++) {
+            if (objects[i] instanceof String) {
+                bytes[i] = objects[i].toString().getBytes();
+            } else {
+                bytes[i] = SerializeObjectTool.serialize(objects[i]);
+            }
+        }
+        return jedis.srem(key.getBytes(), bytes);
+    }
+
+    public Object spop(String key) {
+        byte[] object = jedis.spop(key.getBytes());
+        Object value;
+        try {
+            return value = SerializeObjectTool.unserizlize(object);
+        } catch (UnserizlizeException e) {
+            return value = new String(object);
+        }
+    }
+
+    public Long scared(String key) {
+        return jedis.scard(key.getBytes());
+    }
+
+    public Long smove(String key1, String key2, Object val) {
+        if (val instanceof String) {
+            return jedis.smove(key1, key2, val.toString());
+        } else {
+            return jedis.smove(key1.getBytes(), key2.getBytes(), SerializeObjectTool.serialize(val));
+        }
+    }
+
+    public Set<Object> sinter(String key1, String key2) {
+        Set<Object> result = new HashSet<Object>();
+        Set<byte[]> objects = jedis.sinter(key1.getBytes(), key2.getBytes());
+        for (byte[] obj:objects){
+            try {
+                result.add(SerializeObjectTool.unserizlize(obj));
+            } catch (Exception e) {
+                result.add(new String(obj));
+            }
+        }
+        return result;
+    }
+
+    public Set<Object> sunion(String key1, String key2) {
+        Set<Object> result = new HashSet<Object>();
+        Set<byte[]> objects = jedis.sunion(key1.getBytes(), key2.getBytes());
+        for (byte[] obj : objects) {
+            try {
+                result.add(SerializeObjectTool.unserizlize(obj));
+            } catch (Exception e) {
+                result.add(new String(obj));
+            }
+        }
+        return result;
+    }
+
+    public Set<Object> sdiff(String key1, String key2) {
+        Set<Object> result = new HashSet<Object>();
+        Set<byte[]> objects = jedis.sdiff(key1.getBytes(), key2.getBytes());
+        for (byte[] obj : objects)
+            try {
+                result.add(SerializeObjectTool.unserizlize(obj));
+            } catch (Exception e) {
+                result.add(new String(obj));
+            }
+
+        return result;
+    }
+
+    public Long zadd(String key, Map<String, Double> map) {
+        return jedis.zadd(key, map);
+    }
+
+    public Long zadd(String key, double score, String member) {
+        return jedis.zadd(key, score, member);
+    }
+
+
+    public Set<String> zrange(String key, int i, int j) {
+        return jedis.zrange(key, i, j);
+    }
+
+    public Map<String, Double> zrangeWithScores(String key, int i, int j) {
+        Map<String, Double> result = new HashMap<String, Double>();
+        Set<Tuple> tuples = jedis.zrangeWithScores(key, i, j);
+        for (Tuple tuple : tuples) {
+            result.put(tuple.getElement(), tuple.getScore());
+        }
+        return result;
+    }
+
+    public Set<String> zrangeByScore(String key, double i, double j) {
+        return jedis.zrangeByScore(key, i, j);
+    }
+
+    public Map<String, Double> zrangByScoreWithScores(String key, double i, double j) {
+        Map<String, Double> result = new HashMap<String, Double>();
+        Set<Tuple> tuples = jedis.zrangeByScoreWithScores(key, i, j);
+        for (Tuple tuple : tuples) {
+            result.put(tuple.getElement(), tuple.getScore());
+        }
+        return result;
+    }
+
+    public Double zscore(String key, String value) {
+        return jedis.zscore(key,value);
+    }
+
+    public Long zrank(String key, String value) {
+        return jedis.zrank(key, value);
+    }
+
+    public Long zrem(String key, String... value) {
+        return jedis.zrem(key,value);
+    }
+
+    public Long zcard(String key) {
+        return jedis.zcard(key);
+    }
+
+    public Long zcount(String key, double i, double j) {
+        return jedis.zcount(key,i,j);
+    }
+
+    public Double zincrby(String key, double n, String value) {
+        return jedis.zincrby(key, n, value);
     }
 }
